4ba8e030b46a75c832a16167fa99a5e2ad0da47e
arrKhange1 <therock65frostik@gmail.com>
2023-09-30 15:16:12 +0100

    feat: add parser to divide top header and table, finish top header parser

--- controller.py
+++ controller.py
@@ -1,24 +1,37 @@
-from flask import Flask
+﻿from flask import Flask
 from flask_cors import CORS
 import subprocess
+import os
+import psutil
+import jsonify
+import re
+
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def home():
+    
     return 'hello world'
 
 @app.route("/get-top-data")
 def get_top_data():
+    
+    process = os.popen('top -b -n 1')
+    rawTop = process.read()
+    process.close()
+    
+    rawTopParts = rawTop.split('\n\n')
+    rawTopHeader = rawTopParts[0]
+    rawTopTable = rawTopParts[1]
+    
+    rawTopHeaderArray = re.split('\n', rawTopHeader)
+    rawTopTableArray = re.split('\n', rawTopTable)
+    
+    topHeaderResult = [val.strip() if index != len(rawTopHeaderArray)-1 else val.split('\n\n')[0].strip() for index, val in enumerate(rawTopHeaderArray)]
+    topTableResult = [val.strip() for val in rawTopTableArray]
+    
+    return {'topHeader': topHeaderResult, 'topTable': topTableResult }
 
-    result = []
-    Data = subprocess.check_output(['wmic', 'process', 'list', 'brief'])
-    a = str(Data)
-    try:
-        for i in range(len(a)):
-            result.append(a.split("\\r\\r\\n")[i])
-    except IndexError as e:
-        print("All Done")
-
-    return {'name': result}
+app.run(host="192.168.0.104", port=5000, debug=True)
